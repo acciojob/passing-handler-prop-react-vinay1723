6b0140624af0a9749fdb70aeae77908eff9970c9
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-08-12 22:12:11 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React, { useState } from "react";
+
+export default function Selection({ applyColor }) {
+  const [selectionStyle, updateSelectionStyle] = useState({});
+  function handleClick(e) {
+    e.preventDefault();
+    applyColor(updateSelectionStyle);
+  }
+  return (
+    <div id="box" style={{ ...selectionStyle }} onClick={handleClick}>
+      <p>Selection</p>
+    </div>
+  );
+}
 
